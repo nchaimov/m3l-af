dc40687c5b6de1c74131c9e315a95f54d04b899e
victorhansonsmith <victorhansonsmith@bc65eb7a-1531-11df-8ebe-87dcbb8c21ba>
2009-07-19 22:02:37 +0000

    added skeleton code for annealing.  More annealing code coming soon. . .
    git-svn-id: https://m3l-af.googlecode.com/svn/trunk@68 bc65eb7a-1531-11df-8ebe-87dcbb8c21ba

--- annealing.c
+++ annealing.c
@@ -1,5 +1,66 @@
+#include "utilities.h"
+#include "optimiz.h"
+#include "lk.h"
+#include "free.h"
+#include "models.h"
+#include "mc.h"
+#include "rates.h"
+#include "annealing.h"
+
 //
-// This file will contain methods for thermal and quantum annealing
+argre Get_TA_neighbor_proposition(arbgre *tree){
+
+}
+
+arbre Get_QA_neighbor_proposition(arbre *tree){
+
+}
+
+// INPUT: Given that we're on iteration k, what is the scheduled temperature for this iteration?
 //
+// OUTPUT: a temperature value, as a floating-point decimal
+m3ldbl Temperature(m3ldbl k){
+
+	// Here is where we program the annealing schedule.
+}
+
+// INPUT: Given the energy (e) of our current state, the energy (enew) of our new proposed state,
+// and the temperature (temperature) of the system, calculate the probability of the new state having
+// energy equal to enew, where the probability is drawn from a Boltzmann distribution.
 //
-//
+// OUTPUT: a floating point decimal, between 0.0 and 1.0
+m3ldbl Boltzmann_P(e, enew, m3ldbl temperature){
+
+}
+
+// INPUT: a tree structure, with parameters to optimize specified in tree->mod->s_opt
+// OUTPUT: the likelihood of the best found tree
+m3ldbl Thermal_anneal_all_free_params(arbre *tree, int verbose){
+
+}
+
+// INPUT: a tree structure, with parameters to optimize specified in tree->mod->s_opt
+// OUTPUT: the likelihood of the best found tree
+m3ldbl Quantum_anneal_all_free_params(arbre *tree, int verbose){
+
+	/* Here is psudocode for the algorithm:
+	 * s = input tree with input parameters
+	 * e = the likelihood of s
+	 * sbest = s
+	 * ebest = e
+	 * iter = 0
+	 * while (iter < MAX_QA_ITERS):
+	 * 		snew = Get_QA_neighbor_proposition(s)
+	 * 		enew = likelihood of snew
+	 * 		if enew < ebest:
+	 * 			sbest = snew
+	 * 			ebest = enew
+	 * 		else if Boltzmann_P(e, enew, Temperature(iter / MAX_QA_ITERS) ) > random(0,1):
+	 * 			s = snew
+	 * 			e = enew
+	 * 		iter = iter + 1
+	 * return sbest
+	 */
+
+}
+
