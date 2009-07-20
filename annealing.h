45b5f7a56f495cb3bae24d8fc780c499b8683c03
victorhansonsmith <victorhansonsmith@bc65eb7a-1531-11df-8ebe-87dcbb8c21ba>
2009-07-20 21:03:16 +0000

    git-svn-id: https://m3l-af.googlecode.com/svn/trunk@77 bc65eb7a-1531-11df-8ebe-87dcbb8c21ba

--- annealing.h
+++ annealing.h
@@ -1,7 +1,19 @@
 void Get_TA_neighbor_proposition(arbre *tree);
 void Get_QA_neighbor_proposition(arbre *tree);
-m3ldbl Temperature(m3ldbl k);
 m3ldbl Boltzmann_P(m3ldbl e, m3ldbl enew, m3ldbl temperature);
 m3ldbl Thermal_anneal_all_free_params(arbre *tree, int verbose);
 m3ldbl Quantum_anneal_all_free_params(arbre *tree, int verbose);
+m3ldbl Scale_acceptance_ratio(arbre *tree);
+
+//
+// Global parameters.
+// We should provide the option to set these parameters at runtime,
+// but we should also provide default values.
+//
+m3ldbl temp_count; 	// the number of temperate values to try
+m3ldbl start_temp;	// initial temperature
+m3ldbl end_temp;	// order of magnitude of desired accuracy
+m3ldbl iters_per_temp;	// number of tries at each temperature
+m3ldbl set_back;	// amount to step back if improving
+m3ldbl accept_ratio;	// acceptance ratio
 
