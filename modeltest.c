7745ce8cb3e6578cb33f006f1b9f73ed90731a97
yallreadyforthis <yallreadyforthis@bc65eb7a-1531-11df-8ebe-87dcbb8c21ba>
2010-02-16 03:06:15 +0000

    skeleton of modeltest
    git-svn-id: https://m3l-af.googlecode.com/svn/trunk@196 bc65eb7a-1531-11df-8ebe-87dcbb8c21ba

---
+++ modeltest.c
@@ -0,0 +1,11 @@
+/*
+ *  modeltest.c
+ *  
+ *
+ *  Created by David Elliott on 2/15/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "modeltest.h"
+
---
+++ modeltest.h
@@ -0,0 +1,9 @@
+/*
+ *  modeltest.h
+ *  
+ *
+ *  Created by David Elliott on 2/15/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
