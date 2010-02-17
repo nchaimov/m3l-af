7e2c21db50e83ab92e4d0da2c0f2e3213e353caf
navigatornebular <navigatornebular@bc65eb7a-1531-11df-8ebe-87dcbb8c21ba>
2010-02-17 04:04:54 +0000

    Modeltest.h prototypes
    git-svn-id: https://m3l-af.googlecode.com/svn/trunk@200 bc65eb7a-1531-11df-8ebe-87dcbb8c21ba

--- modeltest.h
+++ modeltest.h
@@ -6,4 +6,12 @@
  *  Copyright 2010 __MyCompanyName__. All rights reserved.
  *
  */
+#ifndef modeltest_h
+#define modeltest_h
 
+#include "utilities.h"
+
+void AIC(arbre* tree);
+float likelihood(arbre* tree, model* mod);
+
+#endif //modeltest_h
